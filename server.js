d12157fbed666b29118f250839cefc18d296f0f9
1181895 <1181895@isep.ipp.pt>
2020-09-08 16:09:21 +0100

    Update

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const server = app.listen (3000, funciton() {
+
+console.log(" Servidor a funcionar na porta 3000", 
+server.address().port);
+   
+});
+
+
